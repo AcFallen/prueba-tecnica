09fe57a131b525df245cef53344b92fcaf1e3d36
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-04-15 21:22:05 -0500

    Modifiqué el DTO de tareas para actualizar el campo de estado a un tipo booleano

--- backend/src/dto/tasks.dto.js
+++ backend/src/dto/tasks.dto.js
@@ -2,11 +2,8 @@ import Joi from 'joi';
 
 export const createTaskDto = Joi.object({
     title: Joi.string().required(),
-    description: Joi.string().required()
 });
 
 export const updateTaskDto = Joi.object({
-    title: Joi.string(),
-    description: Joi.string(),
-    status: Joi.string().valid('PENDING', 'COMPLETED').optional()
+    status: Joi.boolean().required(),
 });
