e20e1da2990dad078fec80ad08cbaef0dee63b27
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-04-16 00:37:20 -0500

    Agregada funcionalidad de socket.io para la creación, actualización y eliminación de tareas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3000");
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -15,6 +18,7 @@ const App = () => {
 
       const jsonData = await response.json();
       setTasks([...tasks, jsonData.content]);
+      socket.emit('create task', jsonData.content);
     } catch (err) {
       console.error(err.message);
     }
@@ -47,6 +51,7 @@ const App = () => {
 
       const jsonData = await response.json();
       setTasks(tasks.map((task) => (task.id === id ? jsonData.content : task)));
+      socket.emit('update task', jsonData.content);
     } catch (err) {
       console.error(err.message);
     }
@@ -63,18 +68,45 @@ const App = () => {
       }
 
       setTasks(tasks.filter((task) => task.id !== id));
+      socket.emit('delete task', id);
     } catch (err) {
       console.error(err.message);
     }
   }
 
   useEffect(() => {
+
     getTasks();
+
+    socket.on('task created', (newTask) => {
+      setTasks((prevTasks) => {
+        // Verificar si la tarea ya existe
+        if (!prevTasks.find((task) => task.id === newTask.id)) {
+          return [...prevTasks, newTask];
+        } else {
+          return prevTasks;
+        }
+      });
+      
+    });
+
+    socket.on('task updated', (updatedTask) => {
+      setTasks((prevTasks) => prevTasks.map((task) => task.id === updatedTask.id ? updatedTask : task));
+    });
+
+    socket.on('task deleted', (id) => {
+      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    });
+
+  
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   return (
     <div>
-      <TodoForm addTask={addTask} />
+      <TodoForm addTask={addTask} socket = {socket}/>
       <TodoList tasks={tasks} checkTask={checkTask} deleteTask={deleteTask} />
     </div>
   );
