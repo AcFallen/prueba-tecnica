211c6b0a9fe6b78543a932ec3fd7af7c7941f494
Roberto Carlos Apaza Cornejo <r.apaza.cornejo@gmail.com>
2024-04-16 00:36:28 -0500

    Agregada funcionalidad de socket.io para la creación, actualización y eliminación de tareas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,43 @@
 import express from 'express';
 import cors from 'cors';
+
+import htttp from 'http';
+import { Server } from 'socket.io';
 import * as Rutas from './routes/index.js';
 
 
 const app = express();
+const server = htttp.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('New connection', socket.id);
+
+  socket.on('create task', (task) => {
+    io.emit('task created', task);
+  });
+
+  socket.on('update task', (task) => {
+    io.emit('task updated', task);
+  });
+
+  socket.on('delete task', (id) => {
+    io.emit('task deleted', id);
+  });
+});
+
 app.use(cors());
+
 app.use(express.json());
 
 app.use(Rutas.taskRouter);
 
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
